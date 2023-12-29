aa44ac50901e84ca916e2097bea83c4721d3b975
Dennis <dennis@noetik.gr>
2023-12-29 13:19:07 +0200

    Sticky navigation with links

--- Connect/wwwroot/scripts/digitalMenu/digitalMenu.js
+++ Connect/wwwroot/scripts/digitalMenu/digitalMenu.js
@@ -1,3 +1,9 @@
+const navLinkEls = document.querySelectorAll('.nav-link');
+const groupEls = document.querySelectorAll('.category');
+const navigation = document.getElementById('menuNav');
+let currentGroup = "home";
+const navInitPosition = navigation.offsetTop;
+
 $(document).ready(() => {
 
     var swiper = new Swiper(".mySwiper", {
@@ -14,4 +20,57 @@ $(document).ready(() => {
         //},
 
     });
+
+
+    //Click Scroll To Section
+    navLinkEls.forEach(navLinkEl => {
+        $(navLinkEl).click(function (event) {
+            event.preventDefault();
+
+            let group_id = navLinkEl.dataset.link
+            group_id = group_id.substring(1)
+            var group = document.getElementById(group_id);
+
+            $([document.documentElement, document.body]).animate({
+                scrollTop: (group.offsetTop - 80)
+            }, 120);
+        });
+    });
+
+    window.addEventListener('scroll', () => {
+
+        if (window.scrollY >= navInitPosition + 10) {
+
+            navigation.classList.add("sticky");
+        }
+        else if (window.scrollY <= navInitPosition ) {
+            navigation.classList.remove("sticky");
+
+        }
+
+        groupEls.forEach(groupEl => {
+
+            if (window.scrollY >= (groupEl.offsetTop - 150)) {
+                currentGroup = groupEl.id;
+            }
+
+        });
+
+        var counter = 0;
+
+        navLinkEls.forEach(navLinkEl => {
+
+            if (navLinkEl.dataset.link.includes(currentGroup)) {
+                document.querySelector('.active').classList.remove('active');
+                navLinkEl.classList.add('active');
+
+                swiper.slideTo(counter);
+            }
+
+            counter++;
+
+        });
+    });
 });
+
+
